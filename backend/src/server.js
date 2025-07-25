3cf5aaca84cc88dd7ff6c3e0895f9c284fd52476
sahaRishav5 <rishavsaha4025@gmail.com>
2025-07-25 12:50:42 +0530

    fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,11 +13,19 @@ const FRONTEND_URL=process.env.FRONTEND_URL;
 const app = express();
 
 const allowedOrigins = ["http://localhost:5173",FRONTEND_URL]
-app.use(cors({
-    origin : allowedOrigins,
-    credentials:true //allow frontend to send cookies
-}))
 app.use(express.json());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(cookieParser())
 
 app.get("/",(req,res)=>{
