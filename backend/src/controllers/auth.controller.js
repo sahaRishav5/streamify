3a20905f4ff787c700e01f17d9b828b063407a6d
Rishav Saha <rishavsaha4025@gmail.com>
2025-08-13 22:22:28 +0530

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -107,7 +107,11 @@ export async function login(req, res) {
 }
 
 export function logout(req, res) {
-  res.clearCookie("jwt");
+  res.clearCookie("jwt", {
+    httpOnly: true,
+    sameSite: "none",
+    secure: true,
+  });
   res.status(200).json({ success: true, message: "Logout successful" });
 }
 
