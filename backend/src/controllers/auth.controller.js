37de1deceeb46d29cb5169d08f45ddf7f6fd0104
Rishav Saha <rishavsaha4025@gmail.com>
2026-08-05 13:00:41 +0530

    Update random avatar URL generation in auth

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -29,8 +29,9 @@ export async function signup(req, res) {
         .json({ message: "Email already exists, please use a diffrent one" });
     }
 
-    const idx = Math.floor(Math.random() * 100) + 1; // generate a num between 1-100
-    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+    const randomAvatar = `https://ui-avatars.com/api/?name=${encodeURIComponent(
+        fullName
+    )}&background=ffffff&color=00000&size=256&rounded=true`;
 
     const newUser = await User.create({
       email,
